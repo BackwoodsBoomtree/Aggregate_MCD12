7f46efe3d06fa37a0105395c0ac43965dbf05144
Doughty <russell.doughty@ou.edu>
2023-02-14 19:48:04 -0600

    Working version

--- combine_tiles.R
+++ combine_tiles.R
@@ -1,57 +1,24 @@
 
 library(terra)
+library(ncdf4)
 
 hdf_input <- "G:/MCD12Q1/original"
 output    <- "G:/MCD12Q1/combined/MCD12Q1.061.2021.nc"
 
 hdf_list <- list.files(hdf_input, pattern = "*.hdf$", full.names = TRUE, recursive = TRUE)
-# 
-# for (i in 1:length(hdf_list)) {
-#   tile <- rast(hdf_list[i])[[1]]
-#   
-#   if (i == 1) {
-#     combo <- tile
-#   } else {
-#     combo <- merge(combo, tile)
-#   }
-# }
 
-collection <- sprc(hdf_list)
+for (i in 1:length(hdf_list)) {
+  tile <- rast(hdf_list[i])[[1]]
 
-combo <- merge(collection)
+  if (i == 1) {
+    combo_list <- list(tile)
+  } else {
+    combo_list <- c(combo_list, tile)
+  }
+  print(paste0("Done with ", i, " of ", length(hdf_list)))
+}
 
-combo <- combo[[1]] # subset for first layer (can not be done during sprc or merge)
+collection <- sprc(combo_list)
+combo      <- merge(collection)
 
-
-writeCDF(combo, filename = output, varname = "Land Cover Type", unit = "", compression = 4, missval = -9999, overwrite = TRUE)
-
-# for (i in 1:length(hdf_list)) {
-#   r <- rast(hdf_list[i])[[1]]
-#   
-#   if (i == 1) {
-#     collection <- sprc(r)
-#   } else {
-#     collection <- c(collection, r)
-#   }
-# }
-
-collection <- sprc(hdf_list)
-
-combo <- merge(collection)
-
-combo <- combo[[1]]
-
-
-hdf_list2 <- hdf_list[1:3]
-
-test <- sprc(hdf_list2, sds = 1)
-
-test2 <- merge(test)
-
-test3 <- subset(test, 1)
-
-test <- sprc(rast(hdf_list[1])[[1]])
-
-test <- rast(hdf_list2[1], xmin = -20015109, xmax = 20015109, ymin = -10007555, ymax= 10007555)[[1]]
-
-, xmin = -20015109, xmax = 20015109, ymin = -10007555, ymax= 10007555
+writeCDF(combo, filename = output, varname = "IGBP Land Cover", unit = "", compression = 4, missval = -9999, overwrite = TRUE)
